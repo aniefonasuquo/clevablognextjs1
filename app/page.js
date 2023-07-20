c88aac654d6408e68258aba15371408b5942ad24
John Asuquo <jaasuquo@hotmail.co.uk>
2023-07-20 18:52:14 +0100

    ...

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import styles from './style.module.css'
+import Image from 'next/image'
 import globe from './../public/landingimages/globex.png'
 import pie from './../public/landingimages/pie-chart.png'
-import incomeimg from './../public/landingimages/incomeone.png'
-import personalityimg from './../public/landingimages/personality.jpg'
-import allocationimg from './../public/landingimages/allocation.png'
-import productdetailimg from './../public/landingimages/productdetail.png'
-import Image from 'next/image'
+import income from './../public/landingimages/incomeone.png'
+import allocation from './../public/landingimages/allocation.png'
+import personality from './../public/landingimages/personality.jpg'
+import productimg from './../public/landingimages/productdetail.png'
 
 export default function Landing () {
 
@@ -136,16 +136,16 @@ c49.899,49.92,115.456,74.859,181.013,74.859s131.115-24.939,181.013-74.859C536.70
     </div>
     <div className={styles.informationillustration}>
       <div className={styles.productimage}>
-        <Image src={incomeimg} width={300} height={300} sizes='100vw'></Image>
+        <Image src={income} width={300} height={300} sizes='100vw'></Image>
       </div>
       <div className={styles.productimage}>
-        <Image src={personalityimg} width={400} height={400} sizes='100vw'></Image>
+        <Image src={personality} width={400} height={400} sizes='100vw'></Image>
       </div>
       <div className={styles.productimage}>
-        <Image src={allocationimg} width={250} height={500} sizes='100vw'></Image>
+        <Image src={allocation} width={250} height={500} sizes='100vw'></Image>
       </div>
       <div className={styles.productimage}>
-        <Image src={productdetailimg} width={300} height={150} sizes='100vw'></Image>
+        <Image src={productimg} width={300} height={150} sizes='100vw'></Image>
       </div>
     </div>
   </section>
